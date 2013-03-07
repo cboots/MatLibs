c556ebb871f147dc43297f325737261ee20c5011
cboots1990 <cboots1990@4facdc8d-61e3-400b-aae7-3533e0e84666>
2013-03-07 17:10:50 +0000

    Improved plot_arrow.
    git-svn-id: https://technomicron/svn/MATLAB%20Libs/trunk@22 4facdc8d-61e3-400b-aae7-3533e0e84666

--- Figures/Graphics/plot_arrow.m
+++ Figures/Graphics/plot_arrow.m
@@ -1,4 +1,4 @@
-function plot_arrow(p0,p1)
+function h = plot_arrow(p0,p1,h)
 %Arrowline 3-D vector plot.
 %   vectarrow(p0,p1) plots a line vector with arrow pointing from point p0
 %   to point p1. The function can plot both 2D and 3D vector with arrow
@@ -18,59 +18,71 @@ function plot_arrow(p0,p1)
 
 %   Rentian Xiong 4-18-05
 %   $Revision: 1.0
+if(nargin < 3)
+    redraw = true;
+else
+    redraw = false;
+end
 
-  if max(size(p0))==3
-      if max(size(p1))==3
-          x0 = p0(1);
-          y0 = p0(2);
-          z0 = p0(3);
-          x1 = p1(1);
-          y1 = p1(2);
-          z1 = p1(3);
-          plot3([x0;x1],[y0;y1],[z0;z1]);   % Draw a line between p0 and p1
-          
-          p = p1-p0;
-          alpha = 0.1;  % Size of arrow head relative to the length of the vector
-          beta = 0.1;  % Width of the base of the arrow head relative to the length
-          
-          hu = [x1-alpha*(p(1)+beta*(p(2)+eps)); x1; x1-alpha*(p(1)-beta*(p(2)+eps))];
-          hv = [y1-alpha*(p(2)-beta*(p(1)+eps)); y1; y1-alpha*(p(2)+beta*(p(1)+eps))];
-          hw = [z1-alpha*p(3);z1;z1-alpha*p(3)];
-          
-          hold on
-          plot3(hu(:),hv(:),hw(:))  % Plot arrow head
-          grid on
-          xlabel('x')
-          ylabel('y')
-          zlabel('z')
-          hold off
-      else
-          error('p0 and p1 must have the same dimension')
-      end
-  elseif max(size(p0))==2
-      if max(size(p1))==2
-          x0 = p0(1);
-          y0 = p0(2);
-          x1 = p1(1);
-          y1 = p1(2);
-          plot([x0;x1],[y0;y1]);   % Draw a line between p0 and p1
-          
-          p = p1-p0;
-          alpha = 0.1;  % Size of arrow head relative to the length of the vector
-          beta = 0.1;  % Width of the base of the arrow head relative to the length
-          
-          hu = [x1-alpha*(p(1)+beta*(p(2)+eps)); x1; x1-alpha*(p(1)-beta*(p(2)+eps))];
-          hv = [y1-alpha*(p(2)-beta*(p(1)+eps)); y1; y1-alpha*(p(2)+beta*(p(1)+eps))];
-          
-          hold on
-          plot(hu(:),hv(:))  % Plot arrow head
-          grid on
-          xlabel('x')
-          ylabel('y')
-          hold off
-      else
-          error('p0 and p1 must have the same dimension')
-      end
-  else
-      error('this function only accepts 2D or 3D vector')
-  end
+if max(size(p0))==3
+    if max(size(p1))==3
+        x0 = p0(1);
+        y0 = p0(2);
+        z0 = p0(3);
+        x1 = p1(1);
+        y1 = p1(2);
+        z1 = p1(3);
+        if(redraw)
+            h(1) = plot3([x0;x1],[y0;y1],[z0;z1]);   % Draw a line between p0 and p1
+        else
+            set(h(1), 'XData', [x0;x1], 'YData', [y0;y1],'ZData',[z0;z1]);
+        end
+        p = p1-p0;
+        alpha = 0.1;  % Size of arrow head relative to the length of the vector
+        beta = 0.1;  % Width of the base of the arrow head relative to the length
+        
+        hu = [x1-alpha*(p(1)+beta*(p(2)+eps)); x1; x1-alpha*(p(1)-beta*(p(2)+eps))];
+        hv = [y1-alpha*(p(2)-beta*(p(1)+eps)); y1; y1-alpha*(p(2)+beta*(p(1)+eps))];
+        hw = [z1-alpha*p(3);z1;z1-alpha*p(3)];
+        
+        hold on
+        if(redraw)
+            h(2) = plot3(hu(:),hv(:),hw(:));  % Plot arrow head
+        else
+            set(h(2), 'XData', hu(:), 'YData', hv(:),'ZData',hw(:));
+        end
+        hold off
+    else
+        error('p0 and p1 must have the same dimension')
+    end
+elseif max(size(p0))==2
+    if max(size(p1))==2
+        x0 = p0(1);
+        y0 = p0(2);
+        x1 = p1(1);
+        y1 = p1(2);
+        if(redraw)
+            h(1) = plot([x0;x1],[y0;y1]);   % Draw a line between p0 and p1
+        else
+            set(h(1), 'XData', [x0;x1], 'YData', [y0;y1]);
+        end
+        p = p1-p0;
+        alpha = 0.1;  % Size of arrow head relative to the length of the vector
+        beta = 0.1;  % Width of the base of the arrow head relative to the length
+        
+        hu = [x1-alpha*(p(1)+beta*(p(2)+eps)); x1; x1-alpha*(p(1)-beta*(p(2)+eps))];
+        hv = [y1-alpha*(p(2)-beta*(p(1)+eps)); y1; y1-alpha*(p(2)+beta*(p(1)+eps))];
+        
+        hold on
+        if(redraw)
+            h(2) = plot(hu(:),hv(:));  % Plot arrow head
+        else
+            set(h(2), 'XData', hu(:), 'YData', hv(:));
+        end
+        hold off
+    else
+        error('p0 and p1 must have the same dimension')
+    end
+else
+    error('this function only accepts 2D or 3D vector')
+end
