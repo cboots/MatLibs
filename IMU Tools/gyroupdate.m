d093c163f92045b38d26c1d175a93fbe7f038469
cboots1990 <cboots1990@4facdc8d-61e3-400b-aae7-3533e0e84666>
2013-03-07 21:42:57 +0000

    git-svn-id: https://technomicron/svn/MATLAB%20Libs/trunk@25 4facdc8d-61e3-400b-aae7-3533e0e84666

--- IMU Tools/gyroupdate.m
+++ IMU Tools/gyroupdate.m
@@ -10,7 +10,7 @@ function rvec = gyroupdate(rvec_prev, gyro, dt)
         q_wdt(1) = cos(phi/2);
         q_wdt(2:4) = sin(phi/2)*u;
         
-        prev_quat = Vect2Quat(rvec_prev');
+        prev_quat = Vect2Quat(reshape(rvec_prev,3,1));
         quat_new = QuatMul(prev_quat, QuatConj(q_wdt));
         
         rvec = Quat2Vect(quat_new);
